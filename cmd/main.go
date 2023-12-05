9ebd2dc9808d3e5c249d7fc93bb49c44cb2fcded
jenovax86 <shruzvn@gmail.com>
2023-12-06 02:11:41 +0330

    feat: Add command-line support

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -28,29 +27,20 @@ func validateFile(fileName string) bool {
 	return false
 }
 
-func rename() {
-	fmt.Println("Enter The filename You want to rename and preferred name")
-	read := bufio.NewReader(os.Stdin)
-
-	input, err := read.ReadString('\n')
-
-	if err != nil {
-		panic(err)
-	}
-
-	input = strings.TrimSuffix(input, "\n")
-	words := strings.Fields(input)
-	if validateFile(words[1]) == true {
+func rename(oldPath string, newPath string) {
+	if validateFile(newPath) == true {
 		panic("This type of character is not allowed when naming a file")
 	}
-	doesFileExist(words[0])
+	doesFileExist(oldPath)
 
-	renameFile := os.Rename(words[0], words[1])
+	renameFile := os.Rename(oldPath, newPath)
 	if renameFile != nil {
 		panic("Error renaming file please try again")
 	}
 }
 
 func main() {
-	rename()
+	rename(os.Args[1], os.Args[2])
+	fmt.Println(os.Args)
 }
+
